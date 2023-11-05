c33a59eb309136902cb647d4c8ae1f98990cb795
Chunliang Mu <chunliang.mu@hdr.mq.edu.au>
2023-11-06 09:48:36 +1100

    Initial Commit.

---
+++ script_PhLocAxes__input.py
@@ -0,0 +1,76 @@
+"""
+Input Parameters for my scripts.
+"""
+
+# imports and internal settings
+import numpy as np
+from astropy import units
+Msun_str = units.Msun.to_string('latex_inline')
+
+
+# script input parameters
+
+iverbose = 3
+
+PHOTOSPHERE_TAU = 1.0
+
+JOB_PROFILES_LIST = (
+    {
+        'job_name': '../photosphere/luis_2md/light',
+        'file_indexes': np.arange(0, 17600+1, 100),
+        'plot_title_suffix': f" for 1.7{Msun_str} primary star with Nucleation Dust",
+        'ieos': 10,
+        'X': 0.691,
+        'name': f"Dusty 1.7{Msun_str}",
+        #'fmt': '-',
+        'color': 'blue',
+    },
+    {
+        'job_name': '../photosphere/luis_4md/light',
+        'file_indexes': np.arange(0, 17600+1, 100),
+        'plot_title_suffix': f" for 3.7{Msun_str} primary star with Nucleation Dust",
+        'ieos': 10,
+        'X': 0.686,
+        'name': f"Dusty 3.7{Msun_str}",
+        #'fmt': '-',
+        'color': 'blue',
+    },
+    {
+        'job_name': '../photosphere/miguel_2m/binary',
+        'file_indexes': np.arange(0, 5000+1, 100),
+        'plot_title_suffix': f" for 1.7{Msun_str} primary star without Dust",
+        'ieos': 10,
+        'X': 0.691,
+        'name': f"Non-dusty 1.7{Msun_str}",
+        #'fmt': '--',
+        'color': 'orange',
+    },
+    {
+        'job_name': '../photosphere/miguel_4m/binary',
+        'file_indexes': np.arange(0, 5000+1, 100),
+        'plot_title_suffix': f" for 3.7{Msun_str} primary star without Dust",
+        'ieos': 10,
+        'X': 0.686,
+        'name': f"Non-dusty 3.7{Msun_str}",
+        #'fmt': '--',
+        'color': 'orange',
+    },
+    {
+        'job_name': '../photosphere/miguel_2m_2022/binary',
+        'file_indexes': np.arange(0, 6000+1, 100),
+        'plot_title_suffix': f" for 1.7{Msun_str} primary star without Dust",
+        'ieos': 10,
+        'X': 0.691,
+        'name': f"Non-dusty 1.7{Msun_str}",
+        #'fmt': '--',
+        'color': 'orange',
+    },
+)
+
+JOB_PROFILES = JOB_PROFILES_LIST[:]
+
+
+JOB_PROFILES_GROUPS = {
+    '2m': (JOB_PROFILES_LIST[0], JOB_PROFILES_LIST[2],),
+    '4m': (JOB_PROFILES_LIST[1], JOB_PROFILES_LIST[3],),
+}
