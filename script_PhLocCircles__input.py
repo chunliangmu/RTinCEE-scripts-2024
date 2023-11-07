4ec010e85a45d64b04e8e4a4d8b95d0db20cd0d6
Chunliang Mu <Mclliang7326@outlook.com>
2023-11-07 13:06:50 +1100

    added func in script_PhLocCircles_analysis to make the movie locally

--- script_PhLocCircles__input.py
+++ script_PhLocCircles__input.py
@@ -3,6 +3,7 @@ Input Parameters for my scripts.
 """
 
 # imports and internal settings
+from _photosphere_jobProfiles__input import JOB_PROFILES_LIST, JOB_PROFILES_GROUPS
 import numpy as np
 from astropy import units
 Msun_str = units.Msun.to_string('latex_inline')
@@ -19,6 +20,13 @@ ray_no = 600
 plane_axes_list = ['xyz', 'xzy']
 
 box_lim = 199
+box_lim_dict = { # using JOB_PROFILES_LIST['nickname'] as key
+    '2md': 199,
+    '4md': 199,
+    '2m' : 39,
+    '4m' : 39,
+    '2m_2022': 39,
+}
 
 fps = 10
 
@@ -31,83 +39,7 @@ unitsOut = {
 use_saved_jsons = False
 
 
-JOB_PROFILES_LIST = (
-    {
-        'job_name': '../photosphere/luis_2md/light',
-        'file_indexes': np.arange(0, 17600+1, 100),
-        'plot_title_suffix': f" for 1.7{Msun_str} primary star with Nucleation Dust",
-        'ieos': 10,
-        'params': {
-            'X' : 0.691,
-            'Z' : 0.021,
-            'mu': 2.381,
-        },
-        'X': 0.691,
-        'name': f"Dusty 1.7{Msun_str}",
-        #'fmt': '-',
-        'color': 'blue',
-    },
-    {
-        'job_name': '../photosphere/luis_4md/light',
-        'file_indexes': np.arange(0, 17600+1, 100),
-        'plot_title_suffix': f" for 3.7{Msun_str} primary star with Nucleation Dust",
-        'ieos': 10,
-        'params': {
-            'X' : 0.686,
-            'Z' : 0.024,
-            'mu': 2.381,
-        },
-        'X': 0.686,
-        'name': f"Dusty 3.7{Msun_str}",
-        #'fmt': '-',
-        'color': 'blue',
-    },
-    {
-        'job_name': '../photosphere/miguel_2m/binary',
-        'file_indexes': np.arange(0, 5000+1, 100),
-        'plot_title_suffix': f" for 1.7{Msun_str} primary star without Dust",
-        'ieos': 10,
-        'params': {
-            'X' : 0.691,
-            'Z' : 0.021,
-            'mu': 2.381,
-        },
-        'X': 0.691,
-        'name': f"Non-dusty 1.7{Msun_str}",
-        #'fmt': '--',
-        'color': 'orange',
-    },
-    {
-        'job_name': '../photosphere/miguel_4m/binary',
-        'file_indexes': np.arange(0, 5000+1, 100),
-        'plot_title_suffix': f" for 3.7{Msun_str} primary star without Dust",
-        'ieos': 10,
-        'params': {
-            'X' : 0.686,
-            'Z' : 0.024,
-            'mu': 2.381,
-        },
-        'X' : 0.686,
-        'name': f"Non-dusty 3.7{Msun_str}",
-        #'fmt': '--',
-        'color': 'orange',
-    },
-    {
-        'job_name': '../photosphere/miguel_2m_2022/binary',
-        'file_indexes': np.arange(0, 6000+1, 100),
-        'plot_title_suffix': f" for 1.7{Msun_str} primary star without Dust",
-        'ieos': 10,
-        'params': {
-            'X' : 0.691,
-            'Z' : 0.021,
-            'mu': 2.381,
-        },
-        'X': 0.691,
-        'name': f"Non-dusty 1.7{Msun_str}",
-        #'fmt': '--',
-        'color': 'orange',
-    },
-)
+
 
 JOB_PROFILES = JOB_PROFILES_LIST[:]
 
