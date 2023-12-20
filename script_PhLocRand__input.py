831566c2a02f1bb7b5511e4b6509607305b8d7bc
Chunliang Mu <chunliang.mu@hdr.mq.edu.au>
2023-12-20 15:01:06 +1100

    Added ability in script_PhLocRand to get dust shell optical depth, inner radius, and photosphere particles radial velocity

--- script_PhLocRand__input.py
+++ script_PhLocRand__input.py
@@ -3,7 +3,7 @@ Input Parameters for my scripts.
 """
 
 # imports and internal settings
-from _photosphere_jobProfiles__input import out_dir, fps, unitsOut, PHOTOSPHERE_TAU, JOB_PROFILES_DICT
+from _photosphere_jobProfiles__input import out_dir, unitsOut, PHOTOSPHERE_TAU, JOB_PROFILES_DICT #, fps
 
 
 verbose = 3
@@ -13,3 +13,5 @@ ray_no = 2000
 # use even number
 #    for binning purposes (since we are plotting abs(cos) figs so the plot is "folded" in half)
 cos_theta_sample_no = 20
+
+fps = 1
