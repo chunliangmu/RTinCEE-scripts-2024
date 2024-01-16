1bba5e2ec7d758297a42ea7af78d57edef1583c6
Chunliang Mu <chunliang.mu@hdr.mq.edu.au>
2024-01-16 15:14:33 +1100

    Beginning writing script to get light curve- initial step

---
+++ script_LCGen__input.py
@@ -0,0 +1,10 @@
+"""
+Input Parameters for my scripts.
+"""
+
+# imports and internal settings
+from _photosphere_jobProfiles__input import interm_dir, output_dir, unitsOut, PHOTOSPHERE_TAU, JOB_PROFILES_DICT #, fps
+
+
+verbose = 3
+
