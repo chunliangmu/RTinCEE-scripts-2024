9a0127512fa6599743971198ae73adf9f23af3f6
Chunliang Mu <chunliang.mu@hdr.mq.edu.au>
2024-10-17 16:39:37 +1100

    Added a script for dust shell optical depth calc

---
+++ script_TauDust__input.py
@@ -0,0 +1,14 @@
+"""
+Input Parameters for my scripts.
+"""
+
+# imports and internal settings
+import numpy as np
+from astropy import units
+from _photosphere_jobProfiles__input import interm_dir, output_dir, unitsOut, PHOTOSPHERE_TAU, JOB_PROFILES_DICT #, fps
+
+
+verbose = 6
+
+job_nicknames = ['2md', '4md']#, '4m', '2m_2022']
+output_dir = f'../fig/20241017_tau-dust/'
