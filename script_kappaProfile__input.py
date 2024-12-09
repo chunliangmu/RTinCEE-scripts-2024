d991ea4d6c0ea8c4c1d7d0f6044a2492fa29e659
Chunliang Mu <chunliang.mu@hdr.mq.edu.au>
2024-12-09 15:50:31 +1100

    Run

--- script_kappaProfile__input.py
+++ script_kappaProfile__input.py
@@ -5,5 +5,6 @@ Input Parameters for my scripts.
 # imports and internal settings
 from _photosphere_jobProfiles__input import fps, unitsOut, output_dir, JOB_PROFILES_DICT
 
+output_dir += f'20231206_kappaProfile_blended-opacity-2/'
 
 verbose = 3
