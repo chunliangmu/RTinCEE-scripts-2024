54ef45dc7b4b23cc3517f48c01980cbe6971db7f
Chunliang Mu <chunliang.mu@hdr.mq.edu.au>
2025-01-24 17:53:21 +1100

    Added a func to gen a list of outter particles from initial dump     and the temperature scale that needed to scale them down to photospheric temp

--- _sharedFuncs.py
+++ _sharedFuncs.py
@@ -7,7 +7,9 @@ Shared helper functions
 
 # imports and internal settings
 import numpy as np
+from numpy import pi
 from astropy import units
+from astropy import constants as const
 from clmuphantomlib.log import is_verbose, say
 from clmuphantomlib import MyPhantomDataFrames, get_eos, get_eos_opacity
 from clmuphantomlib.eos   import EoS_MESA_opacity
@@ -21,6 +23,7 @@ def mpdf_read(
     mpdf: None|MyPhantomDataFrames = None,
     params    : dict = None,
     reset_xyz_by: str='CoM',
+    calc_params :list=['vr', 'R1'],
     kappa_gas : units.Quantity = 2e-4*(units.cm**2/units.g),
     kappa_tol : units.Quantity = 1e-7*(units.cm**2/units.g),
     T_cond_oxy: units.Quantity = 1450 * units.K,
@@ -37,7 +40,7 @@ def mpdf_read(
     if mpdf is None:
         mpdf = MyPhantomDataFrames()
 
-    mpdf.read(job_name, file_index, reset_xyz_by=reset_xyz_by, calc_params=['vr', 'R1'], verbose=verbose)
+    mpdf.read(job_name, file_index, reset_xyz_by=reset_xyz_by, calc_params=calc_params, verbose=verbose)
 
     if eos_opacity is None:
         eos_opacity = get_eos_opacity(ieos=mpdf.ieos, params=params)
@@ -79,3 +82,48 @@ def mpdf_read(
         )
 
     return mpdf
+
+
+
+def gen_Tscales(
+    job_name: str,
+    T_ph : units.Quantity,
+    R_ph : units.Quantity,
+    do_save: bool = True,
+    params : dict = {'X':0.7, 'Z':0.0},
+    verbose: int = 3,
+) -> MyPhantomDataFrames:
+    """Generate scales at t=0 to scale down temperatures of outter particles.
+    
+    So temperatures outside the MESA-calculated photosphere
+    is scaled down to the MESA values.
+    This can be applied to later times,
+    so we can see how much the effect of the instable init
+    of MESA -> Phantom dump can be.
+
+    ---------------------------------------------------------------------------
+    """
+    mpdf = mpdf_read(
+        job_name, file_index=0, reset_xyz_by='', params=params,
+        calc_params=['R1'], verbose=verbose)
+
+    # particle indexes
+    inds = mpdf.get_val('R1') > R_ph
+    scales = np.zeros(
+        np.count_nonzero(inds),
+        dtype=[('iorig', np.int64), ('T_scale', np.float64)])
+    # particle ids
+    #    retrieve back inds from ids by inds_new = mpdf.data['gas']['iorig'].isin(ids)
+    scales['iorig'] = mpdf.data['gas']['iorig'][inds]
+    scales['T_scale'] = (T_ph / mpdf.get_val('T')[inds]).to_value(
+        units.dimensionless_unscaled)
+    # scale down only, do not heat the particles
+    scales['T_scale'][scales['T_scale'] > 1.] = 1.
+
+    if do_save:
+        filename = f"{job_name}_Tscales.npy"
+        say('note', None, verbose, f"Saving to {filename}")
+        with open(filename, 'wb') as fp:
+            np.save(fp, scales)
+
+    return scales
