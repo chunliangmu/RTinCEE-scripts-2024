788090fbb58ae2f60ab6b3af03991ab6d74795f1
Chunliang Mu <chunliang.mu@hdr.mq.edu.au>
2025-01-24 11:43:20 +1100

    Fixes

--- script_LCGen__input.py
+++ script_LCGen__input.py
@@ -14,9 +14,9 @@ verbose = 6
 
 job_nicknames = ['2md', '4md']#, '4m', '2m_2022']
 xyzs_list  = ['xyz', 'xzy']
-no_xy=(64, 64)
+no_xy=(1024, 1024)
 no_xy_txt = 'x'.join([f'{i}' for i in no_xy])
-output_dir = f'../fig/20240222_LCGen/{no_xy_txt}/test_'
+output_dir = f'../fig/20240222_LCGen/{no_xy_txt}/'
 verbose_loop = 0
 
 # - SED settings -
