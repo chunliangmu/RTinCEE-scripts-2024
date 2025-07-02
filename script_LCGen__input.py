85bfc4b2a53f37ba0777baf5941217c5fc7dc314
Chunliang Mu <chunliang.mu@hdr.mq.edu.au>
2025-07-02 17:39:24 +1000

    Attempt to revert LCGen calc change from 35dc9d2e09523cd5cfe5137148bdf3613efa82aa

--- script_LCGen__input.py
+++ script_LCGen__input.py
@@ -22,10 +22,7 @@ output_dir = f'../fig/20240222_LCGen/{no_xy_txt}/'
 interm_dir += 'olim_'
 verbose_loop = 0
 
-sample_midtau        : float = 10.    # middle point tau of sample fraction, where linear scale changes to log scale
-sample_maxtau        : float = 100.   # max tau of sample fraction (must > midtau, preferably >= 2 * midtau)
-nsample_pp           : int   = 1000   # no of sample points for tau in (0., midtau] (linear) and (midtau, maxtau] (log)
-                                      #    total no of sample points is hence twice as nsample_pp
+nsample_pp           : int   = 100   # no of sample points per particle for integration
 
 # at t=0... (only used when use_Tscales=True)
 # numbers from Gonzalez-2024-1
