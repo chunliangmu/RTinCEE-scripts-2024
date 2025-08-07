75d0d40126316f57aa71bcb31ace73c89f8bebfe
Chunliang Mu <chunliang.mu@hdr.mq.edu.au>
2025-08-07 16:18:50 +1000

    Lijiang conference plots gen (pt3)

--- script_LCGen__input.py
+++ script_LCGen__input.py
@@ -19,7 +19,7 @@ xyzs_list  = ['xyz', 'xzy', 'yzx']
 no_xy=(256, 256)
 no_xy_txt = 'x'.join([f'{i}' for i in no_xy])
 output_dir = f'../fig/20240222_LCGen/{no_xy_txt}/'
-interm_dir += 'olim_'
+# interm_dir += 'olim_'
 verbose_loop = 0
 
 nsample_pp  : int   = 1000   # no of sample points per particle for integration
